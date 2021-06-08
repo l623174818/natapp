f47fe41b7e52ee5671725811d28389117e4e81bb
l623174818 <623174818@qq.com>
2021-06-09 02:12:35 +0800

    Updated

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-Wed Jun  9 02:11:17 2021  
-server.natappfree.cc:40673
+Wed Jun  9 02:12:35 2021  
+server.natappfree.cc:38553
 
--- natapp-tcp.bat
+++ natapp-tcp.bat
@@ -1,2 +1,4 @@
 @echo off
-natapp -authtoken=619efe31d8e75603      
+start python natapp_watchdog.py C:\Users\lu\Downloads\natapp_windows_amd64_2_3_9
+ping -n 2 127.0.0.1>nul $exit
+start /wait natapp -authtoken=619efe31d8e75603      
